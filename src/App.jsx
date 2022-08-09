61c3376ce461499735dff2f7198324ba991894f2
mohammed5420 <almoaz065@gmail.com>
2022-08-09 10:26:27 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <h2 className="text-blue-500">framer-studio</h2>
+    </div>
+  );
+}
+
+export default App;
